214b64c6087c69083618e3e8dbdc0ecb7519ac38
Kamsy Umeaku <kamsyumeaku@gmail.com>
2026-07-09 13:13:20 -0400

    fix: fixed the issue with the get and del command type

--- src/command.cpp
+++ src/command.cpp
@@ -37,7 +37,7 @@ string CommandExecutor::execute(const Command& cmd, Store& store) {
             return "+OK\r\n";
 
         case CommandType::GET: {
-            if (cmd.args.empty() or cmd.args.size() < 2) return "-ERR wrong number of arguments\r\n";
+            if (cmd.args.size() != 1) return "-ERR wrong number of arguments\r\n";
             string value;
             if (store.get(cmd.args[0], value)) {
                 return "+" + value + "\r\n";
@@ -46,7 +46,7 @@ string CommandExecutor::execute(const Command& cmd, Store& store) {
         }
 
         case CommandType::DEL:
-            if (cmd.args.empty() or cmd.args.size() <= 2) return "-ERR wrong number of arguments\r\n";
+            if (cmd.args.size() != 1) return "-ERR wrong number of arguments\r\n";
             return store.del(cmd.args[0]) ? ":1\r\n" : ":0\r\n";
 
         case CommandType::EXISTS:
