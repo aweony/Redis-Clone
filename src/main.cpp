dba8e93c1b3c00c3b50a5ffcb201eaa9562df2b0
Kamsy Umeaku <kamsyumeaku@gmail.com>
2026-07-13 23:36:46 -0400

    feat: added snapshot and aof logging to the server

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "server.h"
+#include "store.h"
+#include "aof_logger.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-    Server server("127.0.0.1", 6379);
+    AOFLogger aofLogger("data/appendonly.aof");
+    Store store;
+    store.loadSnapshot("data/dump.rdb");
+    aofLogger.replay(store);
+    aofLogger.open();
+    Server server("127.0.0.1", 6379, store, aofLogger);
     server.start();
     return 0;
 }
